be88acc13bf95059dce026d6abf109f4814259d1
electricman226 <electricman226@gmail.com>
2019-04-05 18:48:10 -0400

    Remove all of our own cheat commands from the copy over.

--- RoRMod/OldCommands.cs
+++ RoRMod/OldCommands.cs
@@ -15,7 +15,7 @@ namespace RoRMod
     {
 
         // Token: 0x06000D11 RID: 3345 RVA: 0x00052C74 File Offset: 0x00050E74
-        [ConCommand( commandName = "god", flags = ( ConVarFlags.ExecuteOnServer | ConVarFlags.Cheat ), helpText = "Toggles god mode on the sender." )]
+        [ConCommand( commandName = "god", flags = ( ConVarFlags.ExecuteOnServer ), helpText = "Toggles god mode on the sender." )]
         private static void CCGod( ConCommandArgs args )
         {
             if ( args.senderMasterObject )
@@ -31,7 +31,7 @@ namespace RoRMod
         }
 
         // Token: 0x06000D18 RID: 3352 RVA: 0x00052FB0 File Offset: 0x000511B0
-        [ConCommand( commandName = "give_money", flags = ( ConVarFlags.ExecuteOnServer | ConVarFlags.Cheat ), helpText = "Gives the specified amount of money to the sender." )]
+        [ConCommand( commandName = "give_money", flags = ( ConVarFlags.ExecuteOnServer ), helpText = "Gives the specified amount of money to the sender." )]
         private static void CCGiveMoney( ConCommandArgs args )
         {
             if ( args.Count == 0 )
@@ -60,7 +60,7 @@ namespace RoRMod
         }
 
         // Token: 0x06000D12 RID: 3346 RVA: 0x00052CDC File Offset: 0x00050EDC
-        [ConCommand( commandName = "zoom", flags = ( ConVarFlags.ExecuteOnServer | ConVarFlags.Cheat ), helpText = "Gives a bunch of items to help travel the map." )]
+        [ConCommand( commandName = "zoom", flags = ( ConVarFlags.ExecuteOnServer ), helpText = "Gives a bunch of items to help travel the map." )]
         private static void CCZoom( ConCommandArgs args )
         {
             if ( args.senderMasterObject )
@@ -75,7 +75,7 @@ namespace RoRMod
         }
 
         // Token: 0x06000D13 RID: 3347 RVA: 0x00052D20 File Offset: 0x00050F20
-        [ConCommand( commandName = "unzoom", flags = ( ConVarFlags.ExecuteOnServer | ConVarFlags.Cheat ), helpText = "Removes the effects of zoom" )]
+        [ConCommand( commandName = "unzoom", flags = ( ConVarFlags.ExecuteOnServer ), helpText = "Removes the effects of zoom" )]
         private static void CCUnzoom( ConCommandArgs args )
         {
             if ( args.senderMasterObject )
@@ -90,7 +90,7 @@ namespace RoRMod
         }
 
         // Token: 0x06000D14 RID: 3348 RVA: 0x00052D64 File Offset: 0x00050F64
-        [ConCommand( commandName = "give_random_items", flags = ( ConVarFlags.ExecuteOnServer | ConVarFlags.Cheat ), helpText = "Gives a random set of items. It will give approximately 80% white, 19% green, 1% red." )]
+        [ConCommand( commandName = "give_random_items", flags = ( ConVarFlags.ExecuteOnServer ), helpText = "Gives a random set of items. It will give approximately 80% white, 19% green, 1% red." )]
         private static void CCGiveRandomItems( ConCommandArgs args )
         {
             if ( args.Count == 0 )
@@ -127,7 +127,7 @@ namespace RoRMod
         }
 
         // Token: 0x06000D15 RID: 3349 RVA: 0x00052E54 File Offset: 0x00051054
-        [ConCommand( commandName = "give_item", flags = ( ConVarFlags.ExecuteOnServer | ConVarFlags.Cheat ), helpText = "Gives the named item to the sender. Second argument can specify stack." )]
+        [ConCommand( commandName = "give_item", flags = ( ConVarFlags.ExecuteOnServer ), helpText = "Gives the named item to the sender. Second argument can specify stack." )]
         private static void CCGiveItem( ConCommandArgs args )
         {
             if ( args.Count == 0 )
@@ -156,7 +156,7 @@ namespace RoRMod
         }
 
         // Token: 0x06000D16 RID: 3350 RVA: 0x00052EE4 File Offset: 0x000510E4
-        [ConCommand( commandName = "inventory_clear", flags = ( ConVarFlags.ExecuteOnServer | ConVarFlags.Cheat ), helpText = "Clears the sender's inventory." )]
+        [ConCommand( commandName = "inventory_clear", flags = ( ConVarFlags.ExecuteOnServer ), helpText = "Clears the sender's inventory." )]
         private static void CCClearItems( ConCommandArgs args )
         {
             if ( args.Count == 0 )
@@ -178,7 +178,7 @@ namespace RoRMod
         }
 
         // Token: 0x06000D17 RID: 3351 RVA: 0x00052F3C File Offset: 0x0005113C
-        [ConCommand( commandName = "give_equipment", flags = ( ConVarFlags.ExecuteOnServer | ConVarFlags.Cheat ), helpText = "Gives the named equipment to the sender." )]
+        [ConCommand( commandName = "give_equipment", flags = ( ConVarFlags.ExecuteOnServer ), helpText = "Gives the named equipment to the sender." )]
         private static void CCGiveEquipment( ConCommandArgs args )
         {
             if ( args.Count == 0 )
@@ -202,7 +202,7 @@ namespace RoRMod
         }
 
         // Token: 0x06001490 RID: 5264 RVA: 0x00070DF4 File Offset: 0x0006EFF4
-        [ConCommand( commandName = "run_set_time", flags = ( ConVarFlags.SenderMustBeServer | ConVarFlags.Cheat ), helpText = "Sets the time of the current run." )]
+        [ConCommand( commandName = "run_set_time", flags = ( ConVarFlags.SenderMustBeServer ), helpText = "Sets the time of the current run." )]
         private static void CCRunSetTime( ConCommandArgs args )
         {
             if ( !Run.instance )
